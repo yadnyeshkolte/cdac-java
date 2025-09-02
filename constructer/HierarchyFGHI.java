adf3909eb870f21173a0376d76f5840736aceb1f
Yadnyesh <yadnyeshkolte@gmail.com>
2025-09-02 18:33:34 +0530

    update

---
+++ constructer/HierarchyFGHI.java
@@ -0,0 +1,24 @@
+package constructer;
+
+class F {
+	int f;
+}
+class G extends F {
+	int g;
+}
+
+class H extends F{
+	int h;
+}
+
+class I extends F implements G,H{
+	
+}
+
+public class HierarchyFGHI {
+	public static void main(String[] args) {
+		
+	}
+}
+
+//errors not valid
