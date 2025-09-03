2e678b1eb14cb77ccb844c8d004391776e2e71f3
Yadnyesh <168337313+yadnyeshkolte@users.noreply.github.com>
2025-09-03 15:55:35 +0530

    Add files via upload

---
+++ assignment4updated/appliance/ApplianceMain.java
@@ -0,0 +1,15 @@
+package assignment4updated.appliance;
+
+public class ApplianceMain {
+	public static void main(String[] args) {
+		Appliance a = new Appliance(); 
+		Fan f = new Fan();
+		AirConditioner ac = new AirConditioner();
+		Refrigerator r = new Refrigerator();
+		
+		a.showConsumption(0);
+		f.showConsumption(20);
+		ac.showConsumption(8);
+		r.showConsumption(24);
+	}
+}
