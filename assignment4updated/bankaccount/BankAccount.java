ee90fb4efd5adae4121e08d764092623f8289cf0
Yadnyesh <168337313+yadnyeshkolte@users.noreply.github.com>
2025-09-03 15:54:47 +0530

    Add BankAccount and interest calculation classes

--- assignment4updated/bankaccount/BankAccount.java
+++ assignment4updated/bankaccount/BankAccount.java
@@ -1,5 +1,43 @@
 package assignment4updated.bankaccount;
 
 public class BankAccount {
+	long accountNumber;
+	float balance;
+	
+	BankAccount(){
+	accountNumber=0000000000;
+	balance=0.0f;
+	}
+	BankAccount(long accountNumber,float balance){
+		this.accountNumber=accountNumber;
+		this.balance=balance;			
+	}
+	void calculateInterest() {
+		System.out.println("Your balance after interest "+balance+balance*0.02);
+	}
 	
 }
+
+class SavingsAccount extends BankAccount{
+	
+	
+	SavingsAccount(long accountNumber, float balance){
+		super(accountNumber, balance);
+	}
+	@Override
+	void calculateInterest() {
+		System.out.println("Your balance after interest in Saving Account "+balance+balance*0.04);
+	}
+	
+}
+
+class FixedDepositAccount extends BankAccount{
+	FixedDepositAccount(long accountNumber, float balance){
+		super(accountNumber, balance);
+	}
+	@Override
+	void calculateInterest() {
+		System.out.println("Your balance after interest in Fixed Deposit Account "+balance+balance*0.07);
+	}
+}
+
