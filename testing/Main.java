0f86e22ccb5a3c81d676378d8d4ffba29b5ef50b
Yadnyesh <yadnyeshkolte@gmail.com>
2025-09-08 18:21:02 +0530

    update

--- testing/Main.java
+++ testing/Main.java
@@ -1,15 +1,43 @@
 package testing;
 
+import java.util.Random;
+import java.util.Arrays;
+
 public class Main {
+
 	public static void main(String[] args) {
-		Student[] testDacs = new Student[6];
-		StoreStudents st = new StoreStudents();
 		
-		testDacs = st.intialise();
+		Random rand = new Random();
 		
-		for(int i=0;i<testDacs.length;i++) {
-			System.out.println("Average of "+testDacs[i].name+" is "+((testDacs[i].emarks+testDacs[i].cmarks+testDacs[i].mmarks)/3));
+		int number = rand.nextInt(100,200);
+		
+		
+		
+		
+		
+		
+		
+		int[] array = new int[20];
+		int[] arr = new int[20];
+
+		for (int number : arr) {
+			// System.out.print(number);
+			number = 3;
+
 		}
-		
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = 100;
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(200);
+		}
+
+		System.out.println("");
+
+		System.out.println(Arrays.toString(arr));
+
+		System.out.println(Arrays.toString(array));
 	}
 }
