16a45e07098081f909a2c25d42dc00665b33ba72
Yadnyesh <yadnyeshkolte@gmail.com>
2025-09-11 22:08:31 +0530

    update

--- questions/productmanagentsystem/PMSMain.java
+++ questions/productmanagentsystem/PMSMain.java
@@ -13,22 +13,39 @@ public class PMSMain {
 		Scanner scan = new Scanner(System.in);
 		int id;
 		String input;
-		//JDBCProductConnections.addProduct();
-		//System.out.print("Enter the id");
-		//id = scan.nextInt();
-		System.out.print("Enter the name");
-		input = scan.next();
-		//JDBCProductConnections.updateDetails(id);
-		
-		/*JDBCProductConnections.viewAllProducts();
-		
-		JDBCProductConnections.calculateTotalInventoryValue();
-		
-		JDBCProductConnections.deleteDetails(id);*/
-		
-		JDBCProductConnections.searchProduct(input);
-		
-		
+		while(true){
+			System.out.println("Enter the case to continue: ");
+			System.out.println("a-add Product , u-update Product, d-delete Product ");
+			System.out.print("v-view Product , s-search Product, c-calculate total price of Inventory ");
+			input = scan.next();
+			if(input.equals("exit")) {
+				break;
+			}
+			switch(input) {
+			case "a": System.out.println("Lets start to add product: ");
+			JDBCProductConnections.addProduct();
+			break;
+			case "u": System.out.print("Enter the id to Update: ");
+			id = scan.nextInt();
+			JDBCProductConnections.updateDetails(id);
+			break;
+			case "d": System.out.print("Enter the id to Delete: ");
+			id = scan.nextInt();
+			JDBCProductConnections.deleteDetails(id);
+			break;
+			case "v": System.out.println("view all products: ");
+			JDBCProductConnections.viewAllProducts();
+			break;
+			case "s": System.out.println("Enter name or category to search the product: ");
+			input = scan.next();
+			JDBCProductConnections.searchProduct(input);
+			break;
+			case "c": System.out.println("calculate inventory ");
+			JDBCProductConnections.calculateTotalInventoryValue();
+			break;
+			default: System.out.println("Invalid Input");
+			}
+		}
 		
 	}
 
